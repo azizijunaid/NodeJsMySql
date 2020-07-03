a61a755adc2d44cdcdfe0a9edd82169822aee1a5
junaidaziz <jaziz@nisum.com>
2020-07-03 11:59:38 +0500

    pagination added

--- app.js
+++ app.js
@@ -10,16 +10,6 @@ const chalk = require("chalk");
 const errorHandler = require("errorhandler");
 const lusca = require("lusca");
 const dotenv = require("dotenv");
-// const MongoStore = require("connect-mongo")(session);
-// const flash = require("express-flash");
-// const path = require("path");
-// const mysql = require("mysql");
-// const passport = require("passport");
-// const expressStatusMonitor = require("express-status-monitor");
-// const sass = require("node-sass-middleware");
-// const multer = require("multer");
-
-// const upload = multer({ dest: path.join(__dirname, "uploads") });
 
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
@@ -29,18 +19,8 @@ dotenv.config({ path: ".env.example" });
 /**
  * Controllers (route handlers).
  */
-// const homeController = require("./controllers/home");
-// const userController = require("./controllers/user");
-// const apiController = require("./controllers/api");
-// const contactController = require("./controllers/contact");
 
 const ordersController = require("./controllers/orders");
-
-/**
- * API keys and Passport configuration.
- */
-// const passportConfig = require("./config/passport");
-// const connection = require("./config/connection");
 
 /**
  * Create Express server.
@@ -48,60 +28,20 @@ const ordersController = require("./controllers/orders");
 const app = express();
 
 /**
- * Connect to MySql.
- */
-// mongoose.set('useFindAndModify', false);
-// mongoose.set('useCreateIndex', true);
-// mongoose.set('useNewUrlParser', true);
-// mongoose.set('useUnifiedTopology', true);
-// mongoose.connect(process.env.MONGODB_URI);
-// mongoose.connection.on('error', (err) => {
-//   console.error(err);
-//   console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
-//   process.exit();
-// });
-
-// const connection = mysql.createConnection({
-//   host: "localhost",
-//   // user: "me",
-//   password: "secret",
-//   database: "my_db",
-// });
-
-// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-//   if (error) throw error;
-//   console.log('The solution is: ', results[0].solution);
-// });
-
-// con.end();
-
-/**
  * Express configuration.
  */
 app.set("host", process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0");
 app.set("port", process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
-// app.set("views", path.join(__dirname, "views"));
-// app.set("view engine", "pug");
-// app.use(expressStatusMonitor());
+
 app.use(compression());
-// app.use(
-//   sass({
-//     src: path.join(__dirname, "public"),
-//     dest: path.join(__dirname, "public"),
-//   })
-// );
+
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(passport.initialize());
-// app.use(passport.session());
+
 app.use(lusca.xframe("SAMEORIGIN"));
 app.use(lusca.xssProtection(true));
 app.disable("x-powered-by");
-// app.use((req, res, next) => {
-//   res.locals.user = req.user;
-//   next();
-// });
 
 /* API'S */
 
@@ -111,9 +51,9 @@ app.post("/orders", ordersController.addOrder); // add order
 
 app.get("/orders/:id", ordersController.getOrderById); //get order by id
 
-app.put("/orders/:id", ordersController.updateOrder); //get order by id
+app.put("/orders/:id", ordersController.updateOrder); //update order by id
 
-app.delete("/orders", ordersController.deleteOrder); //get order by id
+app.delete("/orders/:id", ordersController.deleteOrder); //delete order by id
 
 /**
  * Error Handler.
--- controllers/orders.js
+++ controllers/orders.js
@@ -1,27 +1,24 @@
-// const connection = require("../config/connection");
-// const { result } = require("lodash");
-// const validator = require("validator");
-// const Orders = require("../models/orders.model");
 const db = require("../models/");
-const { order } = require("paypal-rest-sdk");
 
 const {
   Orders,
   Sequelize: { Op },
 } = db;
-// const Op = db.Sequelize.Op;
-exports.getOrders = async (req, res, next) => {
-  // connection.query("SELECT * FROM orders", (err, rows) => {
-  //   if (err) throw err;
-  //   // console.log(rows);
-  //   if (rows.length) {
-  //     res.status(200).json(rows);
-  //   } else {
-  //     res.status(404).json({ error: "not found" });
-  //   }
-  // });
 
+const paginate = ({ page, pageSize }) => {
+  const offset = Number(page * pageSize);
+  const limit = Number(pageSize);
+
+  return {
+    offset,
+    limit,
+  };
+};
+
+exports.getOrders = async (req, res, next) => {
   try {
+    const { page, pageSize } = req.query;
+    const count = await Orders.count({ distinct: true, col: "order_id" });
     const data = await Orders.findAll({
       attributes: [
         "order_id",
@@ -33,8 +30,9 @@ exports.getOrders = async (req, res, next) => {
         "billing_address",
         "logistics_provider",
       ],
+      ...paginate({ page, pageSize }),
     });
-    res.send(data);
+    res.send({ data, totalElements: count });
   } catch (err) {
     res.status(500).send({
       message: err.message || "Some error occurred while retrieving Order.",
@@ -44,15 +42,8 @@ exports.getOrders = async (req, res, next) => {
 
 exports.addOrder = async (req, res) => {
   const { body } = req;
-  // connection.query("INSERT INTO orders SET ?", body, (err, result) => {
-  //   if (err) throw err;
-
-  //   res.status(200).json(result.insertId);
-  // });
-
   try {
     const data = await Orders.create(body);
-    console.log("exports.addOrder -> data", data);
     res.send(data);
   } catch (err) {
     res.status(500).send({
@@ -65,26 +56,8 @@ exports.getOrderById = async (req, res) => {
   const {
     query: { id },
   } = req;
-  // connection.query(
-  //   `SELECT * from orders WHERE order_id=?`,
-  //   id,
-  //   (err, result) => {
-  //     if (err) throw err;
-
-  //     if (result) {
-  //       res.status(200).json(result);
-  //     } else {
-  //       res.status(404).json({ error: "not found" });
-  //     }
-  //   }
-  // );
-
   try {
-    const data = await Orders.findOne({
-      where: {
-        order_id: id,
-      },
-    });
+    const data = await Orders.findByPk(id);
     res.send(data);
   } catch (err) {
     res.status(500).send({
@@ -95,22 +68,9 @@ exports.getOrderById = async (req, res) => {
 
 exports.updateOrder = async (req, res) => {
   const {
-    query: { id },
+    params: { id },
     body,
   } = req;
-  // connection.query(
-  //   "UPDATE `orders` SET ? WHERE `order_id` = ?",
-  //   [body, id],
-  //   (err, result) => {
-  //     if (err) throw err;
-
-  //     if (result) {
-  //       res.status(200).json(result);
-  //     } else {
-  //       res.status(404).json({ error: "not found" });
-  //     }
-  //   }
-  // );
 
   try {
     await Orders.update(body, { where: { order_id: id } });
@@ -124,23 +84,18 @@ exports.updateOrder = async (req, res) => {
 
 exports.deleteOrder = async (req, res) => {
   const {
-    body: { id },
+    params: { id },
   } = req;
-
-  // connection.query(
-  //   "DELETE FROM `orders` WHERE `order_id`=?",
-  //   id,
-  //   (error, results, fields) => {
-  //     if (error) throw error;
-  //     res.status(200).json(results);
-  //   }
-  // );
 
   try {
     const data = await Orders.destroy({ where: { order_id: id } });
-    res.send({ message: "successfully deleted" });
+    if (data === 0) {
+      throw new Error("id not found");
+    } else {
+      res.send({ message: "successfully deleted" });
+    }
   } catch (err) {
-    res.status(500).send({
+    res.status(404).send({
       message: err.message || "Some error occurred while deleting the Order.",
     });
   }
