ecc2600d3e1072ed53a8c5f1fd9ab3f0066f3293
junaidaziz <jaziz@nisum.com>
2020-07-07 17:27:50 +0500

    mysql hosting added

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const app = express();
 /**
  * Express configuration.
  */
-app.set("host", process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0");
+app.set("host", "http://sql12.freemysqlhosting.net" || "0.0.0.0");
 app.set("port", process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
 
 app.use(compression());
