199a5c3a8bd23d2c12f0e59c3231ba79ed314987
junaidaziz <jaziz@nisum.com>
2020-07-07 17:13:46 +0500

    local host update

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
 const dbConfig = {
-  HOST: "localhost" || "http://sql12.freemysqlhosting.net/",
+  HOST: "http://sql12.freemysqlhosting.net/",
   USER: "sql12353213",
   PASSWORD: "K5bhyrDx3f",
-  DB: "sql12353213",
+  DB: "orderdb",
   dialect: "mysql",
   pool: {
     max: 5,
